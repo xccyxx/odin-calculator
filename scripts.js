302c9c5921a8ddc974533ad3af500a2cc6735986
xccyxx <100075841+xccyxx@users.noreply.github.com>
2024-08-27 00:21:27 -0400

    Adjust CSS styles; WIP: Implement JS calculation logic

--- scripts.js
+++ scripts.js
@@ -2,9 +2,26 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-
-let a;
-let operator;
-let b;
-
 const operate = (a, operator, b) => add(a, b);
+
+let a = '';
+let operator = '';
+let b = '';
+let displayValue = '';
+
+// const containClass = (targetElement, cls) => {
+//     if (targetElement.classList.contains(cls))
+//     return targetElement.classList.contains(cls);
+// }
+
+let container = document.querySelector(".container"); 
+let displayBox = document.querySelector("#displayBox");
+
+container.addEventListener("click", event => {
+    let target = event.target
+    if (event.target.classList.contains("number-button")) {
+        a += target.dataset.number;
+        console.log(a);
+        displayBox.innerText = a;
+    }  
+})
